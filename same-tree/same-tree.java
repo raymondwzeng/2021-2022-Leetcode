f133a8d0087fab7ad9dbc8e93a2c604440a06096
Raymond Zeng <rzeng3@gmail.com>
2021-12-29 17:34:59 -0800

    Time: 1 ms (100.00%), Space: 38.4 MB (9.54%) - LeetHub

--- same-tree/same-tree.java
+++ same-tree/same-tree.java
@@ -14,75 +14,59 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-     //2 stack approach, with null checks and such.
-        if(p == null){
-            if (q == null) return true; //Edge case dealing: if p is null, check q.
-            return false;
-        } else if (q == null) {
-            if(p != null) return false; //If q is null and p isn't, return false. We don't need to recheck true, since that should have been already accounted for in the previous statement.
-        }
-        
-        Stack<TreeNode> pStack = new Stack<TreeNode>();
-        Stack<TreeNode> qStack = new Stack<TreeNode>();
+    public Stack<TreeNode> getDFSStack(TreeNode root) {
         TreeNode nullNode = new TreeNode(Integer.MAX_VALUE);
+        Stack<TreeNode> DFS = new Stack<TreeNode>();
+        Stack<TreeNode> result = new Stack<TreeNode>();
         
-        pStack.push(p);
-        qStack.push(q);
-        while(!pStack.isEmpty() && !qStack.isEmpty()) {
-            TreeNode currPNode = pStack.pop();
-            TreeNode currQNode = qStack.pop();
-            
-            if(currPNode == null && currQNode == null) {
-                break; //Break out of the loop if we're dealing with nulls here.
-            } else if ((currPNode != null && currQNode == null) || (currPNode == null && currQNode != null)) {
-                return false;
+        DFS.push(root);
+        result.push(root);
+        
+        while(!DFS.isEmpty()) {
+            TreeNode node = DFS.pop();
+            if(node.left != null) {
+                DFS.push(node.left);
+                result.push(node.left);
+            } else if (node.left == null && node.right != null) {
+                result.push(nullNode);
             }
-            
-            //We don't need additional null checks here because we've checked if one or both are null.
-            if(currPNode.val != currQNode.val) {
-                return false; //If the values aren't the same, then obviously they aren't identical trees.
+            if(node.right != null) {
+                DFS.push(node.right);
+                result.push(node.right);
+            } else if (node.right == null && node.left != null) { //We don't want to push nulls when both children are null (leaves)
+                result.push(nullNode);
             }
-            
-            if(currPNode.right != null || currPNode.left != null) { //If we don't have a leaf node, then we can choose to add nulls.
-                if(currPNode.right == null) {
-                    pStack.push(nullNode);
-                } else if(currPNode.right != null) {
-                    pStack.push(currPNode.right);
-                }
-                if(currPNode.left == null) {
-                    pStack.push(nullNode);
-                } else if (currPNode.left != null) {
-                    pStack.push(currPNode.left);
-                }
-            }
-            if(currQNode.right != null || currQNode.left != null) {
-                if(currQNode.right == null) {
-                    qStack.push(nullNode);
-                } else if(currQNode.right != null) {
-                    qStack.push(currQNode.right);
-                }
-                if(currQNode.left == null) {
-                    qStack.push(nullNode);
-                } else if (currPNode.left != null) {
-                    qStack.push(currQNode.left);
-                }
-            }
-            // pStack.push(currPNode.right);
-            // qStack.push(currQNode.right);
-            // pStack.push(currPNode.left);
-            // qStack.push(currQNode.left);
-            //Apparently, null is allowed. But we need to check for it then.
-            //Question: What about nulls? Well, if it is null, then one stack will empty before the other, and we can compare that outside.
-            //Note: This was *almost* correct. We need to figure out whether a null exists, and I don't *think* stacks handle that natively, so...yeah.
         }
         
-        //If one still has items to traverse while the other doesn't, it's bad. Otherwise, we're good.
-        if((pStack.isEmpty() && !qStack.isEmpty()) || (!pStack.isEmpty() && qStack.isEmpty())) {
+        return result;
+    }
+    
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //Why DFS is better in this case
+        //Honestly, there's not much of a difference IMO.
+        //We still need some "nullNode" because otherwise structure is a bit agnostic.
+        
+        //WHOOPS forgot about edge cases.
+        //3 in total - p null, q null, both null.
+        if(p == null) {
+            if(q == null) return true;
             return false;
-        } else {
-            return true;
+        } else if (q == null) { //Since we already check that p is null before q is null, then if both were null then the first step would have been triggered.
+            return false;
         }
-        //Edge cases: Both null? Well, we'll simply push, uh...null. Whoop. Gotta take care of that.
+        
+        
+        Stack<TreeNode> pDFS = getDFSStack(p);
+        Stack<TreeNode> qDFS = getDFSStack(q);
+        
+        while(!pDFS.isEmpty()) {
+            TreeNode pNode = pDFS.pop(); //No checks needed because it's our while loop condition.
+            if(qDFS.isEmpty()) return false; //They are not the same - one is larger than the other.
+            TreeNode qNode = qDFS.pop();
+            
+            if(pNode.val != qNode.val) return false; //If they are not the same in DFS order, then we can return false.
+        }
+        
+        return (pDFS.isEmpty() && qDFS.isEmpty());
     }
 }
