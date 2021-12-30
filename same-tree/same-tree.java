f568635025e0f58fe4f8bab9ce7f54b468272ab2
Raymond Zeng <rzeng3@gmail.com>
2021-12-29 20:06:06 -0800

    Time: 0 ms (100.00%), Space: 38.3 MB (12.42%) - LeetHub

--- same-tree/same-tree.java
+++ same-tree/same-tree.java
@@ -14,59 +14,15 @@
  * }
  */
 class Solution {
-    public Stack<TreeNode> getDFSStack(TreeNode root) {
-        TreeNode nullNode = new TreeNode(Integer.MAX_VALUE);
-        Stack<TreeNode> DFS = new Stack<TreeNode>();
-        Stack<TreeNode> result = new Stack<TreeNode>();
-        
-        DFS.push(root);
-        result.push(root);
-        
-        while(!DFS.isEmpty()) {
-            TreeNode node = DFS.pop();
-            if(node.left != null) {
-                DFS.push(node.left);
-                result.push(node.left);
-            } else if (node.left == null && node.right != null) {
-                result.push(nullNode);
-            }
-            if(node.right != null) {
-                DFS.push(node.right);
-                result.push(node.right);
-            } else if (node.right == null && node.left != null) { //We don't want to push nulls when both children are null (leaves)
-                result.push(nullNode);
-            }
-        }
-        
-        return result;
-    }
-    
+    //Attempt 3! This time, let's try something recursive.
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        //Why DFS is better in this case
-        //Honestly, there's not much of a difference IMO.
-        //We still need some "nullNode" because otherwise structure is a bit agnostic.
-        
-        //WHOOPS forgot about edge cases.
-        //3 in total - p null, q null, both null.
         if(p == null) {
             if(q == null) return true;
             return false;
-        } else if (q == null) { //Since we already check that p is null before q is null, then if both were null then the first step would have been triggered.
-            return false;
+        } else if (q == null) {
+            return false; //We know from above check that if both null, return true.
         }
-        
-        
-        Stack<TreeNode> pDFS = getDFSStack(p);
-        Stack<TreeNode> qDFS = getDFSStack(q);
-        
-        while(!pDFS.isEmpty()) {
-            TreeNode pNode = pDFS.pop(); //No checks needed because it's our while loop condition.
-            if(qDFS.isEmpty()) return false; //They are not the same - one is larger than the other.
-            TreeNode qNode = qDFS.pop();
-            
-            if(pNode.val != qNode.val) return false; //If they are not the same in DFS order, then we can return false.
-        }
-        
-        return (pDFS.isEmpty() && qDFS.isEmpty());
+        if(p.val != q.val) return false;
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
     }
 }
