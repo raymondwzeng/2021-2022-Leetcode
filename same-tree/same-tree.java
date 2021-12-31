d27d47ba0edc862c888729a06a8c32167bb87380
Raymond Zeng <rzeng3@gmail.com>
2021-12-30 16:47:35 -0800

    Time: 0 ms (100.00%), Space: 36.4 MB (64.38%) - LeetHub

--- same-tree/same-tree.java
+++ same-tree/same-tree.java
@@ -14,15 +14,16 @@
  * }
  */
 class Solution {
-    //Attempt 3! This time, let's try something recursive.
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        //General idea: You can apply the comparisons between nodes on an individual level, and since they HAVE to be the same tree, it's just value comparison.
         if(p == null) {
-            if(q == null) return true;
-            return false;
+            if(q == null) return true; //If both are null, we're ok.
+            return false; //q is not null.
         } else if (q == null) {
-            return false; //We know from above check that if both null, return true.
+            return false; //p is not null.
         }
-        if(p.val != q.val) return false;
-        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        
+        if(p.val != q.val) return false; //The "base case check"
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right)); //Since both trees have to be the same, run an AND.
     }
 }
