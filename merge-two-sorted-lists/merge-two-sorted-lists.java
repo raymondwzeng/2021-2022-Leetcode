6f14c3a3ffd3b229050f009d8bc15a12ccd412e5
Raymond Zeng <rzeng3@gmail.com>
2021-12-31 20:01:10 -0800

    Time: 0 ms (100.00%), Space: 40 MB (5.47%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.java
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,51 +10,53 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        //Two pointer approach, and we'll merge list2 into list1. O(n + m) time (list1 length + list2 length), O(1) space.
         ListNode head = null;
-        ListNode tail = head;
-        ListNode list1Pointer = list1;
-        ListNode list2Pointer = list2;
-        while(list1Pointer != null && list2Pointer != null) {
-            if(list1Pointer.val < list2Pointer.val) {
-                if (head == null) {
-                    head = list1Pointer;
-                    tail = list1Pointer;
+        ListNode tail = null;
+        ListNode list1Runner = list1;
+        ListNode list2Runner = list2;
+        while(list1Runner != null && list2Runner != null) { //We do this because we don't know which one is shorter, and it would take too much to figure out.
+            if(list1Runner.val < list2Runner.val) {
+                if(head == null) {
+                    head = list1Runner;
+                    tail = list1Runner;
                 } else {
-                    tail.next = list1Pointer;
-                    tail = list1Pointer;
+                    tail.next = list1Runner;
+                    tail = list1Runner;
                 }
-                list1Pointer = list1Pointer.next;
+                list1Runner = list1Runner.next;
             } else {
-                if (head == null) {
-                    head = list2Pointer;
-                    tail = list2Pointer;
+                if(head == null) {
+                    head = list2Runner;
+                    tail = list2Runner;
                 } else {
-                    tail.next = list2Pointer; //Whatever our previous tail was, its next is now this.
-                    tail = list2Pointer; //Set our new tail now (attached to end)   
+                    tail.next = list2Runner;
+                    tail = list2Runner;
                 }
-                list2Pointer = list2Pointer.next;
+                list2Runner = list2Runner.next;
             }
         }
-        while(list1Pointer != null) {
-            if(head == null) {
-                head = list1Pointer;
-                tail = list1Pointer;
-            } else {
-                tail.next = list1Pointer;
-                tail = list1Pointer;
+        if(list1Runner == null) {
+            while(list2Runner != null) {
+                if(head == null) {
+                    head = list2Runner;
+                    tail = list2Runner;
+                } else {
+                    tail.next = list2Runner;
+                    tail = list2Runner;
+                }
+                list2Runner = list2Runner.next;
             }
-            list1Pointer = list1Pointer.next;
-        }
-        while(list2Pointer != null) {
-            if(head == null) {
-                head = list2Pointer;
-                tail = list2Pointer;
-            } else {
-                tail.next = list2Pointer;
-                tail = list2Pointer;
+        } else {
+            while(list1Runner != null) {
+                if(head == null) {
+                    head = list1Runner;
+                    tail = list1Runner;
+                } else {
+                    tail.next = list1Runner;
+                    tail = list1Runner;
+                }
+                list1Runner = list1Runner.next;
             }
-            list2Pointer = list2Pointer.next;
         }
         return head;
     }
