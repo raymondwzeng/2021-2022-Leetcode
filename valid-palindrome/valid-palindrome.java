cd40f13ba739d108f691836cd0536de66d5cd4b4
Raymond Zeng <rzeng3@gmail.com>
2021-12-31 20:07:13 -0800

    Time: 31 ms (16.45%), Space: 44 MB (6.62%) - LeetHub

--- valid-palindrome/valid-palindrome.java
+++ valid-palindrome/valid-palindrome.java
@@ -1,13 +1,15 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        //One solution: Copy the clean string, and compare with pointers.
-        //O(s) time, but also O(s) space.
-        s = s.toLowerCase();
-        s = s.replaceAll("[^a-zA-Z0-9]", ""); //Use regex (huh. weird.) to remove whitespace.
-        StringBuffer buffer = new StringBuffer(s);
-        String cpy = buffer.reverse().toString();
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) != cpy.charAt(i)) return false;
+        s = s.toLowerCase(); //Make lower case, possibly O(s)
+        s = s.replaceAll("[^a-zA-Z0-9]", ""); //Also probably O(s)
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right) { //Also O(s/2), and we don't care when they are equal because if left = right, the character is the same
+            if(s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--; //Don't forget to move
         }
         return true;
     }
