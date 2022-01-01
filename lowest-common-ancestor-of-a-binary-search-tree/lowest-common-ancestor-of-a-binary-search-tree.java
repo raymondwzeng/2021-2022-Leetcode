07261dc1a5529b47cc5631827b957eef47b65fbb
Raymond Zeng <rzeng3@gmail.com>
2022-01-01 14:01:13 -0800

    Time: 4 ms (54.10%), Space: 39.5 MB (90.37%) - LeetHub

--- lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
+++ lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.java
@@ -10,63 +10,68 @@
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        //Hmm...this seems like some sort of sorting/searching algorithm? Like going layer by layer?
-        //Then that begs the question - how do we do BFS on a tree?
-        //Again, use a stack? While the stack is not empty, we'll add the children to the stack.
-        //What is the criteria then?
+        Stack<TreeNode> pAncestors = new Stack<>();
+        Stack<TreeNode> qAncestors = new Stack<>();
         
-        //Or maybe we run a BST search for p, q and then compare ancestors?
-        ArrayList<TreeNode> pList = new ArrayList<TreeNode>();
-        ArrayList<TreeNode> qList = new ArrayList<TreeNode>();
+        int pAncestorSize = 0;
+        int qAncestorSize = 0;
         
-        //Start at root.
-        pList.add(root);
-        qList.add(root);
-
-        
-        TreeNode travelNode = root;
-        
-        while(travelNode.val != p.val) { //We can do this since we are given p, q in list.
-            if(travelNode != root) {
-                pList.add(travelNode); //Append the ancestor. (Overengineered)
+        Stack<TreeNode> DFS = new Stack<>();
+        DFS.push(root);
+    
+        //Build pAncestorsTree
+        while(!DFS.isEmpty()) {
+            TreeNode node = DFS.pop();
+            pAncestors.push(node);
+            pAncestorSize++;
+            if(node.val != p.val) {
+                if(node.val < p.val) {
+                    DFS.push(node.right);
+                } else {
+                    DFS.push(node.left);
+                }
             }
-            if(travelNode.val < p.val) {
-                travelNode = travelNode.right; //If we're less than the value, go right.
+        }
+        
+        DFS.push(root);
+        //Build qAncestorsTree
+        while(!DFS.isEmpty()) {
+            TreeNode node = DFS.pop();
+            qAncestors.push(node);
+            qAncestorSize++;
+            if(node.val != q.val) {
+                if(node.val < q.val) {
+                    DFS.push(node.right);
+                } else {
+                    DFS.push(node.left);
+                }
+            }
+        }
+        
+        //Compare the two stacks
+        while(!pAncestors.isEmpty() && !qAncestors.isEmpty()) {
+            if(qAncestorSize < pAncestorSize) {
+            if(qAncestors.peek() == pAncestors.pop()) {
+                return qAncestors.peek();
+                } else {
+                    pAncestorSize--;
+            }
+        } else if (qAncestorSize > pAncestorSize) {
+                if(qAncestors.pop() == pAncestors.peek()) {
+                    return pAncestors.peek();
+                } else {
+                    qAncestorSize--;
+                }
+        } else {
+            TreeNode common = qAncestors.pop();
+            if(common == pAncestors.pop()) {
+                return common;
             } else {
-                travelNode = travelNode.left; //If we're equal or greater, go left.
+                pAncestorSize--;
+                qAncestorSize--;
             }
         }
-        
-        travelNode = root;
-        
-        while(travelNode.val != q.val) { //We can do this since we are given p, q in list.
-            if(travelNode != root) {
-                qList.add(travelNode); //Append the ancestor. (Overengineered)
-            }
-            if(travelNode.val < q.val) {
-                travelNode = travelNode.right; //If we're less than the value, go right.
-            } else {
-                travelNode = travelNode.left; //If we're equal or greater, go left.
-            }
         }
-        
-        pList.add(p); //We need to do this at the end, since they're always the closest/last ancestor to themselves.
-        qList.add(q); //p, q can be descendants of themselves.
-        //Augh, the main problem is that our two lists may not be aligned, I think?
-        //Actually, maybe they do?
-        ArrayList<TreeNode> shorterList = pList;
-        if(pList.size() > qList.size()) {
-            shorterList = qList; //We always check the shorter list since the common ancestor will exist in that one.
-        }
-        
-        TreeNode commonAncestor = root; //Root is common to all.
-        
-        for(int i = 0; i < shorterList.size(); i++) {
-            if(pList.get(i).val == qList.get(i).val) {
-                commonAncestor = shorterList.get(i);
-            }
-        }
-        
-        return commonAncestor;
+        return root;
     }
 }
