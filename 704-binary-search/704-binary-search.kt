49d63c717bd404143545752c7dc1dbe76a42aa1f
raymondwzeng <raymondwzeng@github.com>
2022-06-24 11:18:27 -0700

    57.21% runtime, 66.25% memory

--- 704-binary-search/704-binary-search.kt
+++ 704-binary-search/704-binary-search.kt
@@ -1,13 +1,15 @@
 class Solution {
-    fun searchRecursive(nums: IntArray, target: Int, low: Int, high: Int): Int {
-        if(low > high || low < 0 || high >= nums.size || nums.size == 0) return -1
-        val midpoint = (low + high) / 2
-        if(nums[midpoint] == target) return midpoint
-        if(nums[midpoint] < target) return searchRecursive(nums, target, midpoint + 1, high) //+1 to midpoint is needed to avoid infinite loop with truncation
-        return searchRecursive(nums, target, low, midpoint - 1)
+    fun search(nums: IntArray, target: Int, low: Int, high: Int) : Int {
+        val mid = (low + high) / 2
+        if(low > high || low < 0 || high > nums.size || mid > nums.size - 1) return -1 //Default cases.
+        when {
+            nums[mid] == target -> return mid
+            nums[mid] < target -> return search(nums, target, mid + 1, high)
+            else -> return search(nums, target, low, mid - 1)
+        }
     }
     
     fun search(nums: IntArray, target: Int): Int {
-        return searchRecursive(nums, target, 0, nums.size - 1)
+        return search(nums, target, 0, nums.size)
     }
 }
