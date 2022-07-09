43e6bd3ce343d3ba488b6d18b8fb072c6e845480
raymondwzeng <rzeng3@gmail.com>
2022-07-09 12:34:29 -0700

    11.99% runtime, 29.34% space

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.kt
@@ -0,0 +1,23 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun middleNode(head: ListNode?): ListNode? {
+        //Two pointers pattern
+        var slowPointer = head
+        var fastPointer = head
+        
+        while(fastPointer != null && fastPointer.next != null) {
+            slowPointer = slowPointer?.next
+            fastPointer = fastPointer?.next?.next
+        }
+        
+        return slowPointer
+    }
+}
