fb07f187aa433ef16c385b7b15a71b85aa3e9533
Raymond Zeng <rzeng3@gmail.com>
2022-08-10 12:16:47 -0700

    Time: 259 ms (78.38%), Space: 37.3 MB (36.49%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.kt
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.kt
@@ -13,29 +13,24 @@ class Solution {
         if(s.length == 0) return 0
         var trimmedString = s.trim()
         val numericalRegex = Regex("[0-9]")
-        var sum = 0
+        var sum : Long = 0
         var sign = 1
         var leadingZeroesGone = false
         trimmedString.forEachIndexed { index, char ->
             if(numericalRegex.matches(trimmedString.substring(index, index + 1))) {
                 if(char != '0' || leadingZeroesGone) {
                     if(!leadingZeroesGone) leadingZeroesGone = true
-                    if(sign == -1 && ((sum > Integer.MAX_VALUE / 10) || (sum == Integer.MAX_VALUE / 10 && char - '0' >= 8))) {
-                        return Integer.MIN_VALUE
-                    } else if (sign == 1 && ((sum > Integer.MAX_VALUE / 10) || (sum == Integer.MAX_VALUE / 10 && char - '0' >= 7))) {
-                        return Integer.MAX_VALUE
-                    } else {
                         sum *= 10
                         sum += char - '0'
+                        if(sign == 1 && sum > Integer.MAX_VALUE) return Integer.MAX_VALUE
+                        if(sign == -1 && sum > Integer.MAX_VALUE.toLong() + 1) return Integer.MIN_VALUE
                     }
-                    
-                }
             } else if (index == 0 && (char == '+' || char == '-')) {
                 if(char == '-') sign = -1
             } else {
-                return sum * sign
+                return (sum * sign).coerceIn(Integer.MIN_VALUE.toLong(), Integer.MAX_VALUE.toLong()).toInt()
             }
         }
-        return sum * sign
+        return (sum * sign).coerceIn(Integer.MIN_VALUE.toLong(), Integer.MAX_VALUE.toLong()).toInt()
     }
 }
