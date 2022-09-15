61c8d12915c3cbd7c982b246f09aa32ff0e03268
Raymond Zeng <rzeng3@gmail.com>
2022-09-15 13:58:45 -0700

    Time: 323 ms (10.19%), Space: 34.2 MB (63.21%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.kt
+++ 7-reverse-integer/7-reverse-integer.kt
@@ -12,18 +12,17 @@ class Solution {
             O(logX) time, O(logX) space, where X is the original integer
         */
         if((x >= 0 && x < 10) || (x > -10 && x < 0)) return x
-        val digitsList = LinkedList<Int>()
         val sign = if(x > 0) 1 else -1
         if(x == Integer.MIN_VALUE) return 0 //Corner case - we can't process MIN_VALUE normally at all
         var currentNumber = Math.abs(x)
+        var reverseNumber = 0
         while(currentNumber > 0) { //Break down the integer into digits
-            digitsList.add(currentNumber % 10)
+            val digit = currentNumber % 10
             currentNumber /= 10
+            if((reverseNumber * 10)/10 != reverseNumber) return 0
+            reverseNumber *= 10
+            reverseNumber += digit
         }
-        digitsList.forEachIndexed { index, num ->
-            if((currentNumber * 10)/10 != currentNumber) return@reverse 0 //Overflow for +, - numbers
-            currentNumber = (currentNumber * 10) + num
-        }
-        return currentNumber * sign
+        return reverseNumber * sign
     }
 }
